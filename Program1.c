63137638734e2f5ecf53432b67a55eb4721d4f72
ppaul1 <paul-suchanan@hitmail.com>
2018-03-23 15:44:52 +0700

    changes

--- Program1.c
+++ Program1.c
@@ -2,16 +2,11 @@
 
 int main()
 {
-<<<<<<< HEAD
 	int A = 10, B = 15, C;
 	printf("calculate");
-=======
-	int A = 100, B = 200, C;
 	printf("HaHaHaHaHa");
->>>>>>> 9b625abc5a8a838e4b13830db6e085e493459e93
 	C = A - B;
 	printf("C : %d",C);
-	
 	return 0;
 }
 	
