9b827e47b310938a3a31b88cfca475c399aa5909
storymath <sirasek.j@gmail.com>
2018-03-23 15:28:37 +0700

    Edit

--- Program1.c
+++ Program1.c
@@ -5,7 +5,7 @@ int main()
 	int A = 10, B = 20, C;
 	printf("Hello Wrold");
 	C = A - B;
-	printf("C : ",C);
+	printf("C : %d",C);
 	
 	return 0;
 }
--- Program2.c
+++ Program2.c
@@ -5,7 +5,7 @@ int main()
 	int A = 30, B = 45, C;
 	printf("Addition of 10 + 20 : \n");
 	C = A + B;
-	printf("C : ",C);
+	printf("C : %d",C);
 	
 	return 0;
 }
